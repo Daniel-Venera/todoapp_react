7d7a4fc4582f71d0c36b491d67a8dd930ae7e620
Daniel <daniel.venera2@gmail.com>
2020-05-26 11:06:46 +0200

    Updates

---
+++ precache-manifest.529d2055a67d2b5ea66e1602155f61db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafe3df03458642c6e0c30bd9202b07c",
+    "url": "/todoapp_react/index.html"
+  },
+  {
+    "revision": "52d590a4d532c1e8d5fd",
+    "url": "/todoapp_react/static/css/main.7b1462c5.chunk.css"
+  },
+  {
+    "revision": "0811935ba282ba580665",
+    "url": "/todoapp_react/static/js/2.932e16ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp_react/static/js/2.932e16ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d590a4d532c1e8d5fd",
+    "url": "/todoapp_react/static/js/main.e340252d.chunk.js"
+  },
+  {
+    "revision": "c08960ec08b590181880",
+    "url": "/todoapp_react/static/js/runtime-main.bfd27080.js"
+  }
+]);
