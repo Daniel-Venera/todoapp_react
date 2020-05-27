1bc7ce58e0b7aa4504d8d23dd73a69ee184749b1
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-05-27 17:57:04 +0200

    in modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <br></br>
       <a href='https://github.com/Daniel-Venera/todoapp_react' target='blank'><i class="fa fa-github"></i></a>
       <a href="mailto:daniel.venera2@gmail.com" target="blank"><i class="fa fa-envelope-o"></i></a>
-      <a href="mailto:daniel.venera2@gmail.com" target="blank"><i class="fa fa-linkedin"></i></a>
+      <a href="https://www.linkedin.com/in/daniel-venera/" target="blank"><i class="fa fa-linkedin"></i></a>
     </footer>
   )
 }
